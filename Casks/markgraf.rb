4741ef0ab1c9d59ce32885bdc11173c2ba32340f
Mark <mark.eibes@gmail.com>
2026-05-21 17:16:58 +0200

    markgraf 0.0.10

--- Casks/markgraf.rb
+++ Casks/markgraf.rb
@@ -1,8 +1,8 @@
 cask "markgraf" do
-  version "0.0.9"
-  sha256 "f9ec5826b3bd784141ed7be3422767e446c5cc6c9d2a91046fa60e85754bb8ed"
+  version "0.0.10"
+  sha256 "7b54faffd7bcdd77d5e5155972e69cf69dd61b7c9a053829a3d43a5879b8cb0a"
 
-  url "https://github.com/i-am-the-slime/homebrew-tap/releases/download/v0.0.9/markgraf-darwin-arm64.tar.gz"
+  url "https://github.com/i-am-the-slime/homebrew-tap/releases/download/v0.0.10/markgraf-darwin-arm64.tar.gz"
   name "markgraf"
   desc "Animated graph diagrams CLI"
   homepage "https://github.com/i-am-the-slime/homebrew-tap"
